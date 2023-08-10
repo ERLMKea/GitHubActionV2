4beb47ff76d4e92f3f8d96e46b672ac04e41abe1
Erik Leth Møller <ERLM@kea.dk>
2023-08-10 09:22:58 +0200

    Initial commit

---
+++ src/main/java/com/example/githubactionv2/GitHubActionV2Application.java
@@ -0,0 +1,13 @@
+package com.example.githubactionv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubactionv2/GitHubActionV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
